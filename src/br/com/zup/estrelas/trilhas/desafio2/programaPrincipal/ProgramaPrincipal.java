bf623aea61afdc93c664a0e602a7c2ba9f4c73cd
cristianoSoaresZup <cristiano.soares@zup.com.br>
2021-01-11 20:22:57 -0300

    correção métodos buscar e listar clientes

--- src/br/com/zup/estrelas/trilhas/desafio2/programaPrincipal/ProgramaPrincipal.java
+++ src/br/com/zup/estrelas/trilhas/desafio2/programaPrincipal/ProgramaPrincipal.java
@@ -57,14 +57,25 @@ public class ProgramaPrincipal {
 		} catch (ClienteException e) {
 			System.err.println(e.getMensagemDeErro());
 		}
-		System.out.println(clienteConsultado);
+		System.out.println("nome: " + clienteConsultado.getNome() +  "\n"
+				+ "idade: " + clienteConsultado.getIdade() + "\n"
+						+ "CPF: " + clienteConsultado.getCpf() + "\n"
+								+ "email: " + clienteConsultado.getEmail() + "\n"
+										+ "telefone: " + clienteConsultado.getTelefone() + "\n"
+												+ "endereço: " + clienteConsultado.getEndereco());
 	}
 
 	public static void listaClientes(ClienteDao clienteDao) {
 		try {
 			List<Cliente> clientes = clienteDao.listaClientes();
 			for (Cliente cliente : clientes) {
-				System.out.println(cliente);
+				System.out.println("nome: " + cliente.getNome() +  "\n"
+						+ "idade: " + cliente.getIdade() + "\n"
+								+ "CPF: " + cliente.getCpf() + "\n"
+										+ "email: " + cliente.getEmail() + "\n"
+												+ "telefone: " + cliente.getTelefone() + "\n"
+														+ "endereço: " + cliente.getEndereco() + "\n\n"
+																+ "============================================");
 			}
 		} catch (ClienteException e) {
 			System.err.println(e.getMensagemDeErro());
