9b29e0160f3a56415f257c5f98fa774d92b52ad6
cristianoSoaresZup <cristiano.soares@zup.com.br>
2021-01-29 11:40:54 -0300

    classes ComicMarvel para integracao com API marvel

---
+++ src/main/java/br/com/zup/estrelas/trilhas/nivel1/desafio1/entity/Serie.java
@@ -0,0 +1,17 @@
+package br.com.zup.estrelas.trilhas.nivel1.desafio1.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Serie {
+
+	private String resourceURI;
+	private String name;
+
+}
